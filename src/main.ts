313e35463b344db74dd20745a81f177408332c83
ChuckWagon2480 <ghwar100@gmail.com>
2022-07-06 23:24:46 +0900

    [FIX] enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    // origin: process.env.NODE_ENV == 'dev' ? '*' : process.env.CLIENT_URL,
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
